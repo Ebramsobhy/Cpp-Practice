678e5c78e1ab8814b595290a26375e2eb7ef6abb
Ebram Sobhy <116510911+Ebramsobhy@users.noreply.github.com>
2024-02-14 06:05:48 +0200

    Add files via upload

---
+++ Array of Struct.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct strInfo
+{
+  string FirstName;
+  string LastName;
+  int Age;
+  string Phone;
+};
+
+void ReadInfo(strInfo &Info)
+{
+   cout << "Please Enter FirstName?";
+   cin >> Info.FirstName;
+
+   cout << "Please Enter LastName?";
+   cin >> Info.LastName;
+
+   cout << "Please Enter Age?";
+   cin >> Info.Age;
+
+   cout << "Please Enter Phone?";
+   cin >> Info.Phone;
+}
+
+void PrintInfo(strInfo Info)
+{
+   cout << "\n*********************\n";
+   cout << "FirstName: " << Info.FirstName << endl;
+   cout << "LastName: "  << Info.LastName << endl;
+   cout << "Age: "       << Info.Age << endl;
+   cout << "Phone: "     << Info.Phone << endl;
+}
+
+void ReadPersonsInfo(strInfo Persons[2])
+{
+  ReadInfo(Persons[0]);
+  ReadInfo(Persons[1]);
+}
+
+void PrintPersonsInfo(strInfo Persons[2])
+{
+   cout << "\n******************\n";
+   PrintInfo(Persons[0]);
+   PrintInfo(Persons[1]);
+}
+int main()
+{
+   strInfo Persons[2];
+   ReadPersonsInfo(Persons);
+   PrintPersonsInfo(Persons);
+   
+   return 0;
+}
