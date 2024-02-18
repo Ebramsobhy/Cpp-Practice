7e727367f309b5531740a840cdb0baeaf486ea38
Ebramsobhy <abramsobhy7@gmail.com>
2024-02-18 11:17:06 +0200

    add loop in the functions

--- Array of Struct.cpp
+++ Array of Struct.cpp
@@ -12,16 +12,16 @@ struct strInfo
 
 void ReadInfo(strInfo &Info)
 {
-   cout << "Please Enter FirstName?";
+   cout << "Please Enter FirstName?\n";
    cin >> Info.FirstName;
 
-   cout << "Please Enter LastName?";
+   cout << "Please Enter LastName?\n";
    cin >> Info.LastName;
 
-   cout << "Please Enter Age?";
+   cout << "Please Enter Age?\n";
    cin >> Info.Age;
 
-   cout << "Please Enter Phone?";
+   cout << "Please Enter Phone?\n";
    cin >> Info.Phone;
 }
 
@@ -34,23 +34,33 @@ void PrintInfo(strInfo Info)
    cout << "Phone: "     << Info.Phone << endl;
 }
 
-void ReadPersonsInfo(strInfo Persons[2])
+void ReadPersonsInfo(strInfo Persons[100], int &NumberOfPersons)
 {
-  ReadInfo(Persons[0]);
-  ReadInfo(Persons[1]);
+   cout << "Enter the number of persons? 1 to 100?\n";
+   cin >> NumberOfPersons;
+   for (int i = 0; i <= NumberOfPersons -1; i++)
+   {
+      cout << "Please Enter Person's " << i + 1 << " Info: \n";
+      ReadInfo(Persons[i]);
+   }
 }
 
-void PrintPersonsInfo(strInfo Persons[2])
+void PrintPersonsInfo(strInfo Persons[100], int NumberOfPersons)
 {
    cout << "\n******************\n";
-   PrintInfo(Persons[0]);
-   PrintInfo(Persons[1]);
+   for (int i = 0; i <= NumberOfPersons - 1; i++)
+   {
+       cout << "Persons's " << i + 1 << " Info: \n";
+       PrintInfo(Persons[i]);
+   }
 }
+
 int main()
 {
-   strInfo Persons[2];
-   ReadPersonsInfo(Persons);
-   PrintPersonsInfo(Persons);
+   strInfo Persons[100];
+   int NumberOfPersons = 0;
+   ReadPersonsInfo(Persons, NumberOfPersons);
+   PrintPersonsInfo(Persons, NumberOfPersons);
    
    return 0;
 }
