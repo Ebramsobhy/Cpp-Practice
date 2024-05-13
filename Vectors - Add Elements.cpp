b233cb8e8dc1de6f2f703616629ac3a5c64959b8
Ebramsobhy <abramsobhy7@gmail.com>
2024-05-13 11:57:50 +0300

    Vector - Add Elements

--- Vectors - Add Elements.cpp
+++ Vectors - Add Elements.cpp
@@ -2,11 +2,12 @@
 #include <string>
 #include <vector>
 using namespace std;
-      
-                 // Add Elements To Vectors
-int main() {
 
-    vector <int> vNumbers;
+// Add Elements To Vectors
+int main()
+{
+
+    vector<int> vNumbers;
 
     vNumbers.push_back(10);
     vNumbers.push_back(20);
@@ -21,5 +22,41 @@ int main() {
         cout << Number << endl;
     }
 
+    return 0;
+}
+
+// Another Example
+
+void ReadVectorNumbers(vector <int> &vNumbers)
+{
+    char Choices = 'Y';
+    int Number = 0;
+
+    while (Choices == 'Y' || Choices == 'y')
+    {
+        cout << "Add Number To Vector ";
+        cin >> Number;
+        vNumbers.push_back(Number);
+
+        cout << "Do you want to add more numbers to vector? Y/N ";
+        cin >> Choices;
+    }
+}
+
+void PrintVectorNumbers(vector <int> &vNumbers)
+{
+    cout << "Numbers Vector: \n";
+
+    for (int &Number : vNumbers)
+    {
+        cout << Number << endl;
+    }
+}
+
+int main() {
+
+    vector <int> vNumbers;
+    ReadVectorNumbers(vNumbers);
+    PrintVectorNumbers(vNumbers);
     return 0;
 }
